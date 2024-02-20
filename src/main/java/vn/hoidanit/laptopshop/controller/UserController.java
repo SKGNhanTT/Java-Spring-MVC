4e61c220a67ed3d4d0611c59d543bc9e0d1a23ed
ThanhNhan <tn.nhan98@gmail.com>
2024-02-20 20:53:09 +0900

    view MVC

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -4,22 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.qos.logback.core.model.Model;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello from controller";
-//     }
-
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -28,9 +19,27 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
-    public String getHomePage() {
-        return this.userService.handleHello();
+    @RequestMapping("/")
+    public String getHomePage(org.springframework.ui.Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        return "hello";
     }
 
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
